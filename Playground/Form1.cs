42058219f7a197dbf3509cc6d44ee06504db10d7
Jonathan <Jonathanthu97@gmail.com>
2019-07-12 10:25:23 +0800

    ymujujy7yuj

--- Playground/Form1.cs
+++ Playground/Form1.cs
@@ -16,5 +16,10 @@ namespace Playground
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //kssmsjddjdmd 
+        }
     }
 }
